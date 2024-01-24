d2062f4f2428c36e862da3715e27c7ec7faf7e8f
prashanth-kj <kjprasanth98@gmail.com>
2024-01-24 14:12:28 +0530

    updated

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -12,7 +12,7 @@ function QRCode() {
                 try {
                     e.preventDefault();
                 
-                    let res=await axios.post("https://qrcode-generator-etz5.onrender.com/create",{url})
+                    let res=await axios.post("http://localhost:8000/create",{url})
     
                     console.log(res); 
                     
@@ -33,15 +33,15 @@ function QRCode() {
                         <Grid container spacing={2} className='qrgrid'>
                         <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
                             <TextField label="drop your Link" onChange={(e)=>setUrl(e.target.value)} value={url}/>
-                            <Button className='btn'  onClick={generateQRcode}>Generate QR code</Button>
+                            <Button className='btn' color='success' onClick={generateQRcode}>Generate QR code</Button>
                         </Grid>
-                        <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                        <Grid item xl={4} lg={4} md={6} sm={12} >
                              {
                                  url.length >0 && qrImage
                                  ?
                                  <>
                                    <a href={qrImage} download>  <img src={qrImage} alt="qrcodeimage" /></a>
-                                   <p>Scan the QR Code to access &amp;&amp; Download it!</p>
+                                   <p>Scan the QR Code to access &amp; Download it!</p>
 
                                  </>
                                  :
